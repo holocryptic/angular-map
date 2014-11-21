92c49ba9df6cb98b23c649d09b3eda037f50e7d7
viktoriya.riabovol <viktoriya.riabovol@symphony-solutions.eu>
2014-11-21 19:17:03 +0200

    map aligned

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,55 +1,36 @@
 'use strict';
 
 angular.module('angularMap')
-  .controller('MainCtrl', function ($scope, mapsContent, $document, geolocation, $window) {
+  .controller('MainCtrl', function ($scope, mapsContent, $document) {
 
     $document.ready(function ($) {
-      //Definitions	
+      //Definitions 
       $scope.galleryViewport = $('#gallery-viewport');
       var elWIdth = 205;
       var length = mapsContent.length;
       $scope.width = (length+1) * elWIdth;
       $scope.content = mapsContent;
+
+      //Default Google maps center point
+      $scope.map = { 
+        center: { 
+          latitude: 48, 
+          longitude: 2 
+        }, 
+        zoom: 4 
+      };
       
       //Gallery functionality 
       $scope.currentPhotoIndex = 0;
       $scope.setIndex = function (index) {
         $scope.currentPhotoIndex = index;
+
+        //Change google map center according to active photo on click
+        $scope.map.center = $scope.content[index].Coordinates;
       };
 
       $scope.isCurrentIndex = function (index) {
         return $scope.currentPhotoIndex === index;
-      };
-
-      //Google maps stuff
-      //Default center point
-      $scope.map = { 
-        center: { 
-          latitude: 45, 
-          longitude: -73 
-        }, zoom: 4 
-      };
-      
-
-
-      // get user location
-      $scope.getUserLocation = function() {
-        $window.logGeolocationPermissionReq();
-        geolocation.getLocation().then(function(data) {
-          $scope.currentCoords = {
-            lat  : data.coords.latitude,
-            long : data.coords.longitude
-          };
-
-          // temporarily places us in Hype Park, London, which is useful when developing
-          // appState.coords = {
-          //  lat  : 51.507268,
-          //  long : -0.16573
-          // };
-          console.log( 'was able to get user location: ', $scope.currentCoords );
-        }, function(error) {
-          console.log( error );
-        });
       };
 
     });
